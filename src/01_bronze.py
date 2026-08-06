931faf83d27732b375a1558ee43c3743dc76ff9f
daosmaru3667@gmail.com <daosmaru3667@gmail.com>
2026-08-06 21:35:36 +0000

    refactor bronze layer script
    replace hardcoded file, schema & volume paths with paths from config script

--- src/01_bronze.py
+++ src/01_bronze.py
@@ -2,43 +2,35 @@ import logging
 
 from pyspark.sql.functions import current_timestamp, input_file_name
 
+from utils.config import raw_file_paths, bronze_schema_path
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger("bronze_script")
 
-file_paths = [
-    "crm/cust_info.csv",
-    "crm/prd_info.csv",
-    "crm/sales_details.csv",
-    "erp/CUST_AZ12.csv",
-    "erp/LOC_A101.csv",
-    "erp/PX_CAT_G1V2.csv"
-]
-
-source_prefix = "/Volumes/data_lakehouse/raw/source_systems/"
-target_prefix = "data_lakehouse.bronze."
-
 
 def run_bronze_pipeline():
-    for file_path in file_paths:
-        logger.info(f"Processing file: {source_prefix}{file_path}")
-        table_name = file_path.replace("/", "_").replace(".csv", "").lower()
+    for raw_file_path in raw_file_paths:
+        logger.info(f"Processing file: {raw_file_path}")
+
+        system_name = raw_file_path.parent.name
+        raw_file_name = raw_file_path.name.replace(".csv", "").lower()
+        table_name = f"{system_name}_{raw_file_name}"
 
         try:
             df = (
                 spark.read
                 .option("header", "true")
-                .csv(f"{source_prefix}{file_path}")
+                .csv(f"{raw_file_path}")
             )
 
             df_bronze = df.withColumn("_ingested_at", current_timestamp())
-            df_bronze.write.mode("overwrite").saveAsTable(f"{target_prefix}{table_name}")
-
-            logger.info(f"File processed successfully. Written into: {target_prefix}{table_name}")
+            df_bronze.write.mode("overwrite").saveAsTable(f"{bronze_schema_path}{table_name}")
+            logger.info(f"File processed successfully. Written into: {bronze_schema_path}{table_name}")
         except Exception as e:
-            logger.exception(f"An error occurred while processing file: {source_prefix}{file_path}")
+            logger.exception(f"An error occurred while processing file: {raw_file_path}")
 
 
 if __name__ == "__main__":
