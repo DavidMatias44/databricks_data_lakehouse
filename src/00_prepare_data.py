893d97744f5d58855771ed95ce2007e8d950ddc2
daosmaru3667@gmail.com <daosmaru3667@gmail.com>
2026-08-06 19:59:10 +0000

    refactor preparation script
    wrap logic into function, add logging and error handling

--- src/00_prepare_data.py
+++ src/00_prepare_data.py
@@ -1,10 +1,34 @@
+import logging
 from pathlib import Path
-from shutil import copytree
+from shutil import copytree, Error
 
-project_path = Path.cwd().parent
+from utils.config import raw_data_path, raw_volume_path
 
-data_dir = project_path / "data"
-volume_dir = Path("/Volumes/data_lakehouse/raw/source_systems")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger("preparation")
 
-for directory in ["crm", "erp"]:
-    copytree(data_dir / directory, volume_dir / directory, dirs_exist_ok=True)
+
+def copy_files_into_volume(src_path: Path = raw_data_path, dst_path: Path = raw_volume_path) -> None:
+    """Copy files from the `src_path` into the `dst_path`"""
+
+    if not src_path.exists():
+        raise FileNotFoundError(f"Source path does not exist: {src_path}") 
+    
+    try:
+        for item in src_path.iterdir():
+            if not item.is_dir():
+                continue
+            
+            system_name = item.name
+            logger.info(f"Copying from: {src_path / system_name} to: {dst_path / system_name}")
+            copytree(src_path / system_name, dst_path / system_name, dirs_exist_ok=True)
+            logger.info(f"Success!")
+    except Error as e:
+        logger.exception(f"Unable to copy files due to: {e}")
+
+
+if __name__ == "__main__":
+    copy_files_into_volume()
